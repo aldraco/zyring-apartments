177a321a7ea50594962c6c79927d5f57036620eb
A. Drake <a.l.drake713@gmail.com>
2015-04-27 16:27:33 -0400

    Adding localized images for the cities

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,9 +11,9 @@ angular.module('zyringApp')
   .controller('MainCtrl', function ($scope) {
 
     $scope.cities = [
-    {name: 'Seattle', image: 'http://upload.wikimedia.org/wikipedia/commons/2/2f/Space_Needle002.jpg', ptext: 'Seattle, the coffee capital of the West Coast, offers beautiful natural scenery as well as modern city life.'},
-    {name: 'Tacoma', image: 'http://upload.wikimedia.org/wikipedia/commons/e/e2/Rainier_Port.JPG', ptext: "Mount Ranier and access to Seattle - the best of city and country."},
-    {name: 'Bellevue', image: 'http://media-cdn.tripadvisor.com/media/photo-s/01/d7/66/43/downtown-bellevue-skyline.jpg', ptext: 'Bellevue. Right next to Seattle, without being right in the middle of the city.'}
+    {name: 'Seattle', image: 'images/spaceNeedle.jpg', ptext: 'Seattle, the coffee capital of the West Coast, offers beautiful natural scenery as well as modern city life.'},
+    {name: 'Tacoma', image: 'images/tacomaHeader.JPG', ptext: "Mount Ranier and access to Seattle - the best of city and country."},
+    {name: 'Bellevue', image: 'images/bellevue.jpg', ptext: 'Bellevue. Right next to Seattle, without being right in the middle of the city.'}
     ];
 
     $scope.myInterval = 5000;
