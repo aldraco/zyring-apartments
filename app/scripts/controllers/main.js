275eb5fbb4db4df31451470c97b028bcb8badb76
A. Drake <a.l.drake713@gmail.com>
2015-04-27 16:59:13 -0400

    Style update for home page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,23 +12,12 @@ angular.module('zyringApp')
 
     $scope.cities = [
     {name: 'Seattle', image: 'images/spaceNeedle.jpg', ptext: 'Seattle, the coffee capital of the West Coast, offers beautiful natural scenery as well as modern city life.'},
-    {name: 'Tacoma', image: 'images/tacomaHeader.JPG', ptext: "Mount Ranier and access to Seattle - the best of city and country."},
+    {name: 'Tacoma', image: 'images/tacomaHeader.JPG', ptext: 'Mount Ranier and access to Seattle - the best of city and country.'},
     {name: 'Bellevue', image: 'images/bellevue.jpg', ptext: 'Bellevue. Right next to Seattle, without being right in the middle of the city.'}
     ];
 
-    $scope.myInterval = 5000;
-	  var slides = $scope.slides = [];
-	  $scope.addSlide = function() {
-	    var newWidth = 600 + slides.length + 1;
-	    slides.push({
-	      image: 'http://placekitten.com/' + newWidth + '/300',
-	      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-	        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-	    });
-	  };
-	  for (var i=0; i<4; i++) {
-	    $scope.addSlide();
-	  }
+    
+
 
 
   });
